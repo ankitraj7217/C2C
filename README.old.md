ffd185c7a2b94c1ec39207ef0bcfcab099566154
Rahul Kumar Sinha <Rahulkumar@SSSGMAC-13.local>
2024-01-12 12:29:17 +0530

    1st component

--- README.old.md
+++ README.old.md
@@ -1,1 +1,6 @@
 # C2C
+
+use to create a react basic app `npx create-react-app my-app-name`
+
+if `npm start` gives error
+try `npm install`
---
+++ src/components/Greet.tsx
@@ -0,0 +1,14 @@
+import React,{Component} from 'react'
+
+class Greet extends Component {
+    render() {
+      return (
+        <div>
+          <h1>Hello, React!</h1>
+          <p>Good to see you our very 1st component.</p>
+        </div>
+      );
+    }
+  }
+
+  export default Greet
